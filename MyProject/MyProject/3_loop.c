2bef37b01ddc54a0ee7ce65b3971d513b80b2233
Jaehyun_Park <115146172+vgb30xk@users.noreply.github.com>
2023-04-24 17:02:27 +0900

    4/24
    4

---
+++ MyProject/MyProject/3_loop.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main_loop(void) {
+	// 프로젝트 
+	// 피라미드 만들기
+	int floor;
+	printf("몇 층으로 쌓겠느냐?");
+	scanf_s("%d", &floor);
+
+	for (int i = 0; i < floor; i++)
+	{
+		for (int j = i; j < floor - 1; j++)
+		{
+			printf("S"); // printf(" "); 
+		}
+		for (int k = 0; k < i * 2 + 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
